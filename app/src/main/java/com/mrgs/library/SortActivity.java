5ede0366d2a721b10e43b5b66fb2ffeca1d92992
Samantha929 <17266@students.mrgs.school.nz>
2021-06-25 23:04:27 +1200

    Sort activity and recyclerview for sort activity Image (book cover) for the book to display in sort activity String for the book cover, name, author, year, type, theme

---
+++ app/src/main/java/com/mrgs/library/SortActivity.java
@@ -0,0 +1,115 @@
+package com.mrgs.library;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SortActivity extends AppCompatActivity {
+
+    //Array list
+    private final ArrayList<Integer> mBookCover = new ArrayList<Integer>();
+    private final ArrayList<String> mBookName = new ArrayList<>();
+    private final ArrayList<String> mBookAuthor = new ArrayList<>();
+    private final ArrayList<String> mBookYear = new ArrayList<>();
+    private final ArrayList<String> mBookType = new ArrayList<>();
+    private final ArrayList<String> mBookTheme = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort);
+
+        //Call dataBook() here so it can show in the activity_sort.xml
+        dataBook();
+
+    }
+
+    // Executed when Home button pressed
+    public void openHomefromSort(View v) {
+        Intent home = new Intent(SortActivity.this, com.mrgs.library.MainChatActivity.class);
+        finish();
+        startActivity(home);
+    }
+
+    // Executed when Me button pressed
+    public void openMefromSort(View v) {
+        Intent me = new Intent(SortActivity.this, com.mrgs.library.MeActivity.class);
+        finish();
+        startActivity(me);
+    }
+
+    //Data for the book
+    @SuppressLint("LongLogTag")
+    private void dataBook() {
+        Log.d("Data", "Loading......");
+
+        mBookCover.add(R.drawable.harrypotter_bookcover);
+        mBookName.add("Harry Potter and the Philosopher's Stone");
+        mBookAuthor.add("J.K.Rowling");
+        mBookYear.add("1997");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Fantasy");
+
+        mBookCover.add(R.drawable.thesecretlifeofbees_bookcover);
+        mBookName.add("The Secret Life of Bees");
+        mBookAuthor.add("Sue Monk Kidd");
+        mBookYear.add("2001");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Historical");
+
+        mBookCover.add(R.drawable.yourname_bookcover);
+        mBookName.add("Your Name");
+        mBookAuthor.add("Makoto Shinkai");
+        mBookYear.add("2016");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Love");
+
+        mBookCover.add(R.drawable.thegiver_bookcover);
+        mBookName.add("The Giver");
+        mBookAuthor.add("Lois Lowry");
+        mBookYear.add("1993");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Suspense");
+
+        mBookCover.add(R.drawable.trash_bookcover);
+        mBookName.add("Trash");
+        mBookAuthor.add("Andy Mulligan");
+        mBookYear.add("2010");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Imaginary");
+
+        mBookCover.add(R.drawable.hanako_bookcover);
+        mBookName.add("Jibaku Shonen Hanako-kun");
+        mBookAuthor.add("Aidairo");
+        mBookYear.add("2015");
+        mBookType.add("Manga, Fiction");
+        mBookTheme.add("Imaginary");
+
+        mBookCover.add(R.drawable.thehungergames_bookcover);
+        mBookName.add("The Hunger Games");
+        mBookAuthor.add("Suzanne Collins");
+        mBookYear.add("2008");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Adventure");
+
+        recyclerview();
+    }
+
+    //Set the book to display in the recycler view
+    private void recyclerview() {
+        Log.d("recycler view", "Recycler view");
+        RecyclerView recyclerView = findViewById(R.id.sort_recyclerview);
+        SortRecyclerViewActivity adapter = new SortRecyclerViewActivity(this,
+                mBookCover, mBookName, mBookAuthor, mBookYear, mBookType, mBookTheme);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ app/src/main/java/com/mrgs/library/SortRecyclerViewActivity.java
@@ -0,0 +1,96 @@
+package com.mrgs.library;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class SortRecyclerViewActivity extends RecyclerView.Adapter<SortRecyclerViewActivity.ViewHolder> {
+
+    //Array list
+    private ArrayList<Integer> mBookCover = new ArrayList<>();
+    private ArrayList<String> mBookName = new ArrayList<>();
+    private ArrayList<String> mBookAuthor = new ArrayList<>();
+    private ArrayList<String> mBookYear = new ArrayList<>();
+    private ArrayList<String> mBookType = new ArrayList<>();
+    private ArrayList<String> mBookTheme = new ArrayList<>();
+    private final Context mContext;
+
+    public SortRecyclerViewActivity(Context context, ArrayList<Integer> bookCover, ArrayList<String> bookName,
+                                    ArrayList<String> bookAuthor, ArrayList<String> bookYear,
+                                    ArrayList<String> bookType, ArrayList<String> bookTheme) {
+
+        mBookCover = bookCover;
+        mBookName = bookName;
+        mBookAuthor = bookAuthor;
+        mBookYear = bookYear;
+        mBookType = bookType;
+        mBookTheme = bookTheme;
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_sort_recyclerview_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.d("view holder", "View holder called");
+        Glide.with(mContext).asBitmap().load(mBookCover.get(position)).into(holder.nBookCover);
+
+        holder.nBookName.setText(mBookName.get(position));
+        holder.nBookAuthor.setText(mBookAuthor.get(position));
+        holder.nBookYear.setText(mBookYear.get(position));
+        holder.nBookType.setText(mBookType.get(position));
+        holder.nBookTheme.setText(mBookTheme.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBookName.size();
+
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //Constant
+        ImageView nBookCover;
+        TextView nBookName;
+        TextView nBookAuthor;
+        TextView nBookYear;
+        TextView nBookType;
+        TextView nBookTheme;
+        LinearLayout nSortRecyclerViewLayout;
+
+
+        //Item view
+        public ViewHolder (View itemView) {
+            super(itemView);
+            nBookCover = itemView.findViewById(R.id.book_cover);
+            nBookName = itemView.findViewById(R.id.book_name);
+            nBookAuthor = itemView.findViewById(R.id.book_author);
+            nBookYear = itemView.findViewById(R.id.book_year);
+            nBookType = itemView.findViewById(R.id.book_type);
+            nBookTheme = itemView.findViewById(R.id.book_theme);
+            nSortRecyclerViewLayout = itemView.findViewById(R.id.sort_recyclerview_layout);
+
+        }
+    }
+}
