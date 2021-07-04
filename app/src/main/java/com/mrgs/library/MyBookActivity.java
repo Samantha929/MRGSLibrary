b80445df9e4947f179b50868b9adf8447f886d86
Samantha929 <17266@students.mrgs.school.nz>
2021-07-04 15:34:06 +1200

    Add Issued button and Overdue button in the MyBookActivity and then add two different recyclerview to these two buttons

--- app/src/main/java/com/mrgs/library/MyBookActivity.java
+++ app/src/main/java/com/mrgs/library/MyBookActivity.java
@@ -1,17 +1,35 @@
 package com.mrgs.library;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MyBookActivity extends AppCompatActivity {
+
+    //Array list
+    private final ArrayList<Integer> mBookCover = new ArrayList<Integer>();
+    private final ArrayList<String> mBookName = new ArrayList<>();
+    private final ArrayList<String> mBookAuthor = new ArrayList<>();
+    private final ArrayList<String> mBookYear = new ArrayList<>();
+    private final ArrayList<String> mBookType = new ArrayList<>();
+    private final ArrayList<String> mBookTheme = new ArrayList<>();
+    public static int i=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mybook);
+
+        //Call dataBook() here so it can show in the activity_sort.xml
+        dataBook();
     }
 
     // Executed when Home button pressed
@@ -33,5 +51,107 @@ public class MyBookActivity extends AppCompatActivity {
         Intent me = new Intent(MyBookActivity.this, com.mrgs.library.MeActivity.class);
         finish();
         startActivity(me);
+    }
+
+    //Data for the book
+    @SuppressLint("LongLogTag")
+    private void dataBook() {
+        Log.d("Data", "Loading for data......");
+
+        mBookCover.add(R.drawable.harrypotter_bookcover);
+        mBookName.add("Harry Potter and the Philosopher's Stone");
+        mBookAuthor.add("J.K.Rowling");
+        mBookYear.add("1997");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Fantasy");
+
+        mBookCover.add(R.drawable.thesecretlifeofbees_bookcover);
+        mBookName.add("The Secret Life of Bees");
+        mBookAuthor.add("Sue Monk Kidd");
+        mBookYear.add("2001");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Historical");
+
+        mBookCover.add(R.drawable.yourname_bookcover);
+        mBookName.add("Your Name");
+        mBookAuthor.add("Makoto Shinkai");
+        mBookYear.add("2016");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Love");
+
+        mBookCover.add(R.drawable.thegiver_bookcover);
+        mBookName.add("The Giver");
+        mBookAuthor.add("Lois Lowry");
+        mBookYear.add("1993");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Suspense");
+
+        mBookCover.add(R.drawable.trash_bookcover);
+        mBookName.add("Trash");
+        mBookAuthor.add("Andy Mulligan");
+        mBookYear.add("2010");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Imaginary");
+
+        mBookCover.add(R.drawable.hanako_bookcover);
+        mBookName.add("Jibaku Shonen Hanako-kun");
+        mBookAuthor.add("Aidairo");
+        mBookYear.add("2015");
+        mBookType.add("Manga, Fiction");
+        mBookTheme.add("Imaginary");
+
+        mBookCover.add(R.drawable.thehungergames_bookcover);
+        mBookName.add("The Hunger Games");
+        mBookAuthor.add("Suzanne Collins");
+        mBookYear.add("2008");
+        mBookType.add("Novel, Fiction");
+        mBookTheme.add("Adventure");
+
+        recyclerview();
+    }
+
+    //Set the book to display in the recycler view
+    private void recyclerview() {
+        Log.d("recycler view", "Connecting to recycler view");
+
+        //Find the variables from the id in xml
+        RecyclerView issuedRecyclerView = findViewById(R.id.issued_recyclerview);
+        RecyclerView overdueRecyclerView = findViewById(R.id.overdue_recyclerview);
+
+        //If function for when end-user click Issued button or Overdue button
+        if (i==1){
+            //Set the visibility for the recycler view
+            issuedRecyclerView.setVisibility(View.VISIBLE);
+            overdueRecyclerView.setVisibility(View.GONE);
+
+            //The adapter for the issued recyclerview
+            IssuedRecyclerViewActivity adapter = new IssuedRecyclerViewActivity(this,
+                    mBookCover, mBookName, mBookAuthor, mBookYear, mBookType, mBookTheme);
+            issuedRecyclerView.setAdapter(adapter);
+            issuedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+        else if (i==2){
+            //Set the visibility for the recycler view
+            overdueRecyclerView.setVisibility(View.VISIBLE);
+            issuedRecyclerView.setVisibility(View.GONE);
+
+            //The adapter for the issued recyclerview
+            OverdueRecyclerViewActivity adapter = new OverdueRecyclerViewActivity(this,
+                    mBookCover, mBookName, mBookAuthor, mBookYear, mBookType, mBookTheme);
+            overdueRecyclerView.setAdapter(adapter);
+            overdueRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        }
+    }
+
+    // Executed when Issued button pressed
+    public void issuedBook(View v){
+        i = 1;
+        recyclerview();
+    }
+
+    // Executed when Overdue button pressed
+    public void overdueBook(View v){
+        i = 2;
+        recyclerview();
     }
 }
---
+++ app/src/main/java/com/mrgs/library/OverdueRecyclerViewActivity.java
@@ -0,0 +1,103 @@
+package com.mrgs.library;
+
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class OverdueRecyclerViewActivity extends RecyclerView.Adapter<OverdueRecyclerViewActivity.ViewHolder> {
+
+    //Array list
+    private ArrayList<Integer> mBookCover = new ArrayList<>();
+    private ArrayList<String> mBookName = new ArrayList<>();
+    private ArrayList<String> mBookAuthor = new ArrayList<>();
+    private ArrayList<String> mBookYear = new ArrayList<>();
+    private ArrayList<String> mBookType = new ArrayList<>();
+    private ArrayList<String> mBookTheme = new ArrayList<>();
+    private final Context mContext;
+
+    //Constructor
+    public OverdueRecyclerViewActivity(Context context, ArrayList<Integer> bookCover, ArrayList<String> bookName,
+                                       ArrayList<String> bookAuthor, ArrayList<String> bookYear,
+                                       ArrayList<String> bookType, ArrayList<String> bookTheme) {
+
+        mBookCover = bookCover;
+        mBookName = bookName;
+        mBookAuthor = bookAuthor;
+        mBookYear = bookYear;
+        mBookType = bookType;
+        mBookTheme = bookTheme;
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //Recycling the view holder to put in the position where I want to place in
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_overdue_recyclerview_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Log.d("view holder", "View holder called");
+
+        //Hold book cover in the position I want to placed
+        Integer nBookCover = mBookCover.get(position);
+        Drawable drawable = mContext.getResources().getDrawable(nBookCover);
+
+        //Hold to the book data in the position I want to placed
+        holder.nBookCover.setImageDrawable(drawable);
+        holder.nBookName.setText(mBookName.get(position));
+        holder.nBookAuthor.setText(mBookAuthor.get(position));
+        holder.nBookYear.setText(mBookYear.get(position));
+        holder.nBookType.setText(mBookType.get(position));
+        holder.nBookTheme.setText(mBookTheme.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        //To tell how item in the list need to be display in the app
+        return mBookName.size();
+
+    }
+
+    //Store each book's data using ViewHolder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        //Constant
+        ImageView nBookCover;
+        TextView nBookName;
+        TextView nBookAuthor;
+        TextView nBookYear;
+        TextView nBookType;
+        TextView nBookTheme;
+        LinearLayout nOverdueRecyclerViewLayout;
+
+
+        //Item view
+        public ViewHolder (View itemView) {
+            super(itemView);
+            //Find the variables from the id in xml and make it into item view
+            nBookCover = itemView.findViewById(R.id.overdue_book_cover);
+            nBookName = itemView.findViewById(R.id.overdue_book_name);
+            nBookAuthor = itemView.findViewById(R.id.overdue_book_author);
+            nBookYear = itemView.findViewById(R.id.overdue_book_year);
+            nBookType = itemView.findViewById(R.id.overdue_book_type);
+            nBookTheme = itemView.findViewById(R.id.overdue_book_theme);
+            nOverdueRecyclerViewLayout = itemView.findViewById(R.id.overdue_recyclerview_layout);
+
+        }
+    }
+}
